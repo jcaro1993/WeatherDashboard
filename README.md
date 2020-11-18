df249871dee6a3c7b293bdbe96bd23db28a731c4
Javier Caro <jcaro1993@gmail.com>
2020-11-17 21:47:43 -0800

    add read.me

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # WeatherDashboard
+can search any city in the USA and it will give you the Temperature, Humidity, and wind-speed.
--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var today = new Date();
+var date = today.getDate();
+var month = today.getMonth() + 1;
+var year = today.getFullYear();
 $(document).ready(() => {
     function weatherInfo() {
         var city = $("#city-input").val()
@@ -12,33 +16,33 @@ $(document).ready(() => {
             $('#temperature').text("Temperature: " + res.list[0].main.temp)
             $('#wind-speed').text("wind-speed:" + res.list[0].wind.speed)
             $('#humidity').text("humidity:" + res.list[0].main.humidity)
+            var lati = res.city.coord.lat
+            var longt = res.city.coord.lon
             console.log(res)
+            console.log(longt)
+            console.log(lati)
+
+            var apiQuery2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lati + "&lon=" + longt + "&units=imperial&appid=caf932346b78dde952075fa3fc6aed80"
+            $.ajax({
+                url: apiQuery2,
+                method: "GET"
+            }).then(function(res2) {
+                console.log(res2)
+                for (i = 1; i < 6; i++) {
+                    var days = $("<div class='col forecast bg-primary text-white ml-3 mb-3 rounded'>")
+                    var date5 = date++ + 1
+                    var forecaste = $("<h5>").text(" (" + month + "/" + date5 + "/" + year + ")")
+                    var temp = $("<p>").text("Temp: " + Math.round(res2.daily[i].temp.day) + "F")
+                    var conds = $("<p>").text("Conditions: " + res2.daily[i].weather[0].main)
+                    var humi = $("<p>").text("Humidity: " + res2.daily[i].humidity + "%")
+                    $(".5day").append(days)
+                    $(days).append(forecaste, temp, conds, humi)
+
+                }
+
+            })
         })
     }
-
     $("#search-button").on("click", weatherInfo)
+
 })
-
-
-// var queryURL = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=caf932346b78dde952075fa3fc6aed80"
-// $.ajax({
-//     header: origin,
-//     url: queryURL,
-//     method:"GET"
-// }).then(function(response){
-//     for(var i = 3; i < 36; i=i+8){
-//         var temp = (((1.8)*(response.list[i].main.temp-273)+32).toFixed(2)).toString()
-//         var date = response.list[i].dt_txt.substring(0,10)
-//         var weather = response.list[i].weather[0].icon
-//         $(".date"+i).html(date)
-//         $(".temp"+i).html("Temperature: " + temp + "°F")
-//         $(".humidity"+i).html("Humidity: " + response.list[i].main.humidity + "%")
-//         var icon = $("<img src = 'http://openweathermap.org/img/wn/" + weather + ".png'>")
-//         $(".icon"+i).html("")
-//         $(".icon"+i).append(icon)
-//     }
-//     console.log(weather)
-
-
-
-// })
